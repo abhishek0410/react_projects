9c1de9a0715947d80e2f140d8872a40b19159ab9
Abhishek <u6411540@anu.edu.au>
2019-03-11 13:37:28 +1100

    The #todolist# project has been paused for a while.The reason is that this tododlist app made extensive use of graphql and some new syntax associated with the graph ql .On the other hand we want to focus on what we already know..SO at this point the current todolist app will be paused...We will infact start over with another todolist which uses express in place of graphql-yoga

---
+++ todolist_mern/server/server.js
@@ -0,0 +1,26 @@
+const { GraphQLServer } = require("graphql-yoga");
+const mongoose = require("mongoose");
+
+const Todo = mongoose.model("Todo", {
+  text: String,
+  complete: Boolean
+});
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || "World"}`
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() =>
+    server.start(() => console.log("Server is running on localhost:4000"))
+  );
+// ... or using `require()`
